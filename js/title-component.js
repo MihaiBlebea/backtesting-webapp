b3a27c7ff97cd1a83698a2727982d270499c8259
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-04-03 19:40:45 +0100

    Adds axios and vue

--- js/title-component.js
+++ js/title-component.js
@@ -1,10 +1,22 @@
-import { createApp } from 'vue'
-	
+import { createApp } from "vue"
+import axios from "axios"
+
+const BASE_URL = "http://192.168.1.17:8080/api/v1"
+
 createApp({
+	template: `<button type="button" v-on:click="createAccount" class="btn btn-primary">Create Account</button>`,
 	data() {
 		return {
-			message: 'Hello Vue!'
+			message: "Hello Vue!"
 		}
 	},
-	template: `<div>The message is {{ message }}</div>`
-}).mount('#app')
+	methods: {
+		createAccount: ()=> {
+			axios.post(BASE_URL + "/account").then((result)=> {
+				console.log(result.data)
+			}).catch((e)=> {
+				console.error(e)
+			})
+		}
+	},
+}).mount("#app")
