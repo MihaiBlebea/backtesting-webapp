f3e3aaf18d8b4c15800edb9180488228247639c8
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-04-09 15:32:07 +0100

    Change api url

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://127.0.0.1:8081"
+// export const BASE_URL = "http://127.0.0.1:8081"
 
-// export const BASE_URL = "http://192.168.1.17:8080/api/v1"
+export const BASE_URL = "https://trading-platform.cap-rover.purpletreetech.com/api/v1"
 
 export const getQueryParams = ()=> {
 	let query = window.location.search.replace("?", "")
