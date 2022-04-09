764956a50e03b09f07010bd4b6dce5759041a1d1
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-04-09 14:53:10 +0100

    Fix paths

--- server-static.sh
+++ server-static.sh
@@ -3,7 +3,4 @@
 npx live-server \
 	-port=8080 \
 	--ignore=./mock \
-	--mount=/:./html \
-	--mount=/css:./css \
-	--mount=/js:./js \
-	--mount=/assets:./assets
+	--mount=/:./public
--- server.sh
+++ server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# npx json-server ./mock/index.js -p 8081
 node ./mock/index.js
